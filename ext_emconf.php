7f21b7b75007523b10e2e55f8932defefacce125
Angela Dudtkowski <dudtkowski@clickstorm.de>
2018-05-17 14:56:59 +0200

    [FEATURE] support powermail 3.8

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '7.6.0-7.6.99',
-            'powermail' => '3.9',
+            'powermail' => '3.0',
             'vhs' => '2.4'
         ],
         'conflicts' => [
